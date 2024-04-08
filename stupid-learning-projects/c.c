2e8f5e046a7f25267bc82c44b3296ee551bf7861
2salted <noamgraf123@gmail.com>
2024-04-08 10:45:59 +0000

    update

--- stupid-learning-projects/c.c
+++ stupid-learning-projects/c.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 int main() {
+  int intMain;
   int studentAge = 13;
   float studentLoan = 104.2;
   int studentID = 96;
   char studentGrade = 'B';
   printf("Joe mama is %d and has student loans of %f\nJoe mama has a grade of "
-         "%c and a student id: %d",
+         "%c and a student id: %d\n",
          studentAge, studentLoan, studentGrade, studentID);
+  printf("%lu", sizeof(intMain));
 
   return 0;
 }
